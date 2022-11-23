09cd87d4993ccb94db7cba41a5c45c6b7d0d7664
DongiCoding <Hello.git.loveehd012@gmail.com>
2022-11-23 17:09:46 +0900

    UserService.java, UserServiceImpl.java, UserDAO.java, user-mapper.xml are updated

--- _999_ReviewUs/src/main/java/com/ezen/springboard/service/user/impl/UserServiceImpl.java
+++ _999_ReviewUs/src/main/java/com/ezen/springboard/service/user/impl/UserServiceImpl.java
@@ -4,9 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ezen.springboard.service.user.UserService;
+import com.ezen.springboard.vo.UserVO;
 
 @Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserDAO userDAO;
+
+	@Override
+	public int idCheck(String userId) {
+		return userDAO.idCheck(userId);
+	}
+
+	@Override
+	public int nmCheck(String userNm) {
+		return userDAO.nmCheck(userNm);
+	}
+
+	@Override
+	public int join(UserVO userVO) {
+		return userDAO.join(userVO);
+	}
+
+	@Override
+	public UserVO login(UserVO userVO) {
+		return userDAO.login(userVO);
+	}
 }
